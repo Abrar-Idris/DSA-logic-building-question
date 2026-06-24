839e27bf5255f2923282d9d4c978b2d6ac9b129e
abrar-idrisi <abrarcoding3334@gmail.com>
2026-06-24 11:37:10 +0530

    Day 30 DSA practice

--- checkArrayIsSorted.java
+++ checkArrayIsSorted.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-//count Elements greater than given Number
-public class CountElementsGreaterGivenNumber {
-    
+
+public class checkArrayIsSorted {
     public static void main(String[] args) {
+        
          Scanner sc = new Scanner(System.in);
         System.out.println("Enter length of First array: ");
         int n = sc.nextInt();
@@ -13,15 +13,18 @@ public class CountElementsGreaterGivenNumber {
             arr[i] =sc.nextInt();
         }
 
-        System.out.print("Enter the treget Number: ");
-        int treget =sc.nextInt();
-        int count=0;
-
-        for(int i = 0; i<n; i++){
-            if(treget<arr[i]){
-                count++;
+         boolean isSorted= true;
+        for(int i =0; i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+              isSorted = false; 
+              break;
             }
         }
-        System.out.println("Number of greater elements of compare to target  : "+count);
+
+        if(isSorted){
+          System.out.println("Array is sorted ");
+        }else{
+            System.out.println("Array is Not sorted");
+        }
     }
 }
