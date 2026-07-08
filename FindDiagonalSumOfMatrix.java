21d35025821e80c0df04b37276b70cad2f76bb9e
abrar-idrisi <abrarcoding3334@gmail.com>
2026-07-08 22:20:37 +0530

    Day 39 Diagonal Sum of matrix

---
+++ FindDiagonalSumOfMatrix.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FindDiagonalSumOfMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Row length : ");
+        int rows =sc.nextInt();
+
+        System.out.println("Enter the Columns length : ");
+        int cols =sc.nextInt();
+
+        int [][]arr =new int[rows][cols];
+
+        for(int i =0; i<rows;i++){
+            for(int j=0; j<cols; j++){
+             arr[i][j]=sc.nextInt();
+            }
+        }
+
+        int diagonal =0;
+        for(int i =0; i<rows; i++){
+            for(int j = 0; j<cols; j++){
+              if(arr[i]==arr[j]){
+                diagonal += arr[i][j];
+              }
+            }
+        }
+        System.out.println("Diagonal Sum : : "+diagonal);
+    }
+}
