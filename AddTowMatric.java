159c2fb91f4e3626c427ec9d752f32253fdbc8f0
abrar-idrisi <abrarcoding3334@gmail.com>
2026-07-21 12:24:04 +0530

    Day 44 add tow Matrix

--- AddTowMatric.java
+++ AddTowMatric.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
-//Day 43 DSA practice
-//check if two matrices are equal1
 
-public class CheckTwoMatricesAreEqual {
+public class AddTowMatric {
   public static void main(String[] args) {
-
     Scanner sc = new Scanner(System.in);
 
+    
     //first matrix 
 
      System.out.print("Enter Rows: ");
@@ -15,13 +13,13 @@ public class CheckTwoMatricesAreEqual {
         System.out.print("Enter Columns: ");
         int cols1 = sc.nextInt();
 
-        int[][] arr1 = new int[row1][cols1];
+        int[][] matrixA = new int[row1][cols1];
 
         System.out.println("Enter Elements:");
 
         for (int i= 0; i < row1; i++) {
             for (int j = 0; j < cols1; j++) {
-                arr1[i][j] = sc.nextInt();
+                matrixA[i][j] = sc.nextInt();
             }
         }
 
@@ -33,40 +31,36 @@ public class CheckTwoMatricesAreEqual {
         System.out.print("Enter second matrics Columns: ");
         int cols2 = sc.nextInt();
 
-        int[][] arr2 = new int[row2][cols2];
+        int[][] matrixB = new int[row2][cols2];
 
         System.out.println("Enter Elements:");
 
         for (int i= 0; i < row2; i++) {
             for (int j = 0; j < cols2; j++) {
-                arr2[i][j] = sc.nextInt();
+                matrixB[i][j] = sc.nextInt();
             }
         }
 
         //logic
-        if(row1 != row2 || cols1 != cols2){
-            System.out.println("Matrices length isNot equal");
-        return;
-        }
-        
-        boolean equal = true;
 
-        for(int i = 0; i<row1; i++){
-            for(int j = 0; j<cols1; j++){
-                if(arr1[i][j]!=arr2[i][j]){
-                 equal = false;
-                 break;
-                }
+        if(row1 != row2 || cols1 != cols2){
+            System.out.println("Matrix addition is not possible");
+            return;
+        }
+
+        int[][] matrixSum = new int[row1][cols1];
+
+        for(int i = 0; i < row1; i++){
+            for(int j = 0; j < cols1; j++){
+                matrixSum[i][j]=matrixA[i][j] + matrixB[i][j];
             }
         }
-
-        if(equal){
-            System.out.println("Both Matrices are equal ");
-        }else{
-            System.out.println("both matrices are not equal");
+       System.out.println("Matrix1 + matrix 2 = sum");
+        for(int i = 0; i<row1; i++){
+            for(int j =0; j<cols1; j++){
+                System.out.print(matrixSum[i][j]+" ");
+            }
+            System.out.println();
         }
-    
-    
-
   }  
 }
