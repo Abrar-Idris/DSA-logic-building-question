772bfd2bdfd92901f03c5e93413aa5c55c46afee
abrar-idrisi <abrarcoding3334@gmail.com>
2026-07-26 13:11:14 +0530

    day 49 P,S diagonal sum

---
+++ SumOfPrimeAndSceondaryDiagonalOfSqure.java
@@ -0,0 +1,45 @@
+
+//find the sum of primary and secondary diagonal of 
+// squre matrix
+
+import java.util.Scanner;
+
+public class SumOfPrimeAndSceondaryDiagonalOfSqure {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the Row length : ");
+        int rows =sc.nextInt();
+
+        System.out.println("Enter the Columns length : ");
+        int cols =sc.nextInt();
+
+        int [][]arr =new int[rows][cols];
+
+        for(int i =0; i<rows;i++){
+            for(int j=0; j<cols; j++){
+             arr[i][j]=sc.nextInt();
+            }
+        }
+        if(rows!=cols){
+         System.out.println("it is not a squre matrix ");
+         return;
+        }
+        int primaryDiagonalSum = 0;
+        int secondaryDiagonalSum = 0;
+        //logic
+        for(int i = 0; i<rows;i++){
+         for(int j = 0; j<cols; j++){
+            if(i==j){
+                primaryDiagonalSum += arr[i][j];
+            }
+            if(i+j == cols-1){
+              secondaryDiagonalSum += arr[i][j];
+            }
+         }
+        }
+
+        System.out.println("Sum of primary diagonal matrix : "+primaryDiagonalSum);
+        System.out.println("sum of secondary diagonal matrix : "+secondaryDiagonalSum);
+   } 
+   } 
