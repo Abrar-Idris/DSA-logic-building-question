307414f61783e225e2af734e963a2ca923781dcd
abrar-idrisi <abrarcoding3334@gmail.com>
2026-07-28 11:57:26 +0530

    Day 51 minmum in 2D array

---
+++ findMinmumElementInTwoDarray.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+//find the minmum element in a 2D matrix
+//day 51
+
+public class findMinmumElementInTwoDarray {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the row length : ");
+    int rows =sc.nextInt();
+
+    System.out.print("Enter the columns length : ");
+    int cols =sc.nextInt();
+
+    int arr[][]=new int[rows][cols];
+
+    for(int i =0; i<rows; i++){
+        for(int j = 0; j<cols; j++){
+          arr[i][j]=sc.nextInt();
+        }
+    }
+     System.out.println();
+
+    for(int i =0; i<rows; i++){
+        for(int j = 0; j<cols; j++){
+          System.out.print(arr[i][j]+" ");
+        }
+        System.out.println();
+    }
+
+    //logic 
+    int minmum = arr[0][0];
+ for(int i =0; i<rows; i++){
+        for(int j = 0; j<cols; j++){
+         if (arr[i][j]<minmum) {
+            minmum =arr[i][j];
+         }
+        }
+    }
+    
+System.out.println("The Minmum Element is "+minmum);
+    }
+}
