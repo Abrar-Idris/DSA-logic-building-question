3b1e7ca4eb918d8628dec737e49bd5e50ff872df
abrar-idrisi <abrarcoding3334@gmail.com>
2026-07-30 12:38:55 +0530

    day 54 Sum of each Columns In 2D

---
+++ findSumofColumnInTwoDarray.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class findSumofColumnInTwoDarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the row length : ");
+    int rows =sc.nextInt();
+
+    System.out.print("Enter the columns length : ");
+    int cols =sc.nextInt();
+
+    int arr[][]=new int[rows][cols];
+
+    for(int i =0; i<rows; i++){
+        for(int j = 0; j<cols; j++){
+          arr[i][j]=sc.nextInt();
+        }
+    }
+
+     System.out.println();
+
+    for(int i =0; i<rows; i++){
+        for(int j = 0; j<cols; j++){
+          System.out.print(arr[i][j]+" ");
+        }
+        System.out.println();
+    }
+
+    //logic
+    for(int j =0; j<cols; j++){
+        int sum = 0;
+        for(int i = 0; i<rows; i++){
+          sum += arr[i][j];
+        }
+        System.out.println("columns "+j+" sum = "+sum);
+    }
+    }
+}
