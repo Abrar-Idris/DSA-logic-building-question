bc8c8d1c90fdd80099a5747dcfd090a28adf4eb3
abrar-idrisi <abrarcoding3334@gmail.com>
2026-08-19 18:57:13 +0530

    find Column with Max value Of even element

---
+++ findColumnWithMaxNumberOfEvenElement.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+//day 68 
+//find the column with maximum number of even Element 
+
+public class findColumnWithMaxNumberOfEvenElement {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the row lenght : ");
+    int rows =sc.nextInt();
+
+    System.out.print("Enter the cols lenght : ");
+    int cols =sc.nextInt();
+
+    int arr[][]=new int[rows][cols];
+//input
+    for(int i =0; i<rows; i++){
+        for(int j =0; j<cols; j++){
+            arr[i][j] = sc.nextInt();
+        }
+       
+    }
+//print
+     for(int i =0; i<rows; i++){
+        for(int j =0; j<cols; j++){
+            System.out.print(" "+arr[i][j]);
+        }
+       System.out.println();
+    }
+
+    //logic
+    int countElement= 0;
+    int maximumEvenNumberColumn= 0;
+
+    for(int j =0; j<cols; j++){
+
+        int evenElementInColumn = 0;
+
+        for(int i =0; i<rows; i++){
+            
+         if(arr[i][j]%2==0){
+            evenElementInColumn++;
+         }    
+        }
+
+        if (countElement<evenElementInColumn) {
+            countElement = evenElementInColumn;
+            maximumEvenNumberColumn =j;
+        }
+       
+    }
+
+    System.out.println("MaximumEven Number in this column "+maximumEvenNumberColumn);
+
+}
+}
