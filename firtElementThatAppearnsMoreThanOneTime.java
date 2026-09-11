7169ed4ef076b0e492301e3e03db9f1e882a3cbe
abrar-idrisi <abrarcoding3334@gmail.com>
2026-09-11 22:00:09 +0530

    firstRepeatingElement

---
+++ firtElementThatAppearnsMoreThanOneTime.java
@@ -0,0 +1,45 @@
+/*given an integer array,find the first element that
+appearns more than once 
+ */
+
+import java.util.Scanner;
+
+public class firtElementThatAppearnsMoreThanOneTime {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the length of array : ");
+    int n =sc.nextInt();
+
+    int arr[]=new int[n];
+
+    System.out.println("enter the element ");
+    for(int i =0; i<n; i++){
+        arr[i]=sc.nextInt();
+    }
+
+    //logic 
+   
+    boolean found =false;
+
+   for(int i =0; i<n-1; i++){
+    for(int j =0; j<i; j++){
+    
+        if(arr[i]==arr[j]);
+        {
+         System.out.println("first repeating element: "+arr[i]);
+        found = true;
+        break;
+        }
+    }
+    if(found){
+        break;
+    }
+   }
+
+    if(!found){
+   System.out.println("no repeating element ");
+    }
+    sc.close();
+    }
+}
